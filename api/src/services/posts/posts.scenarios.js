c6a9f08dc0035d7d51a2a17f1019ff78e245d6c4
NandaScott <nanda1123@gmail.com>
2022-07-08 17:49:56 -0400

    Chapter 2 complete

---
+++ api/src/services/posts/posts.scenarios.js
@@ -0,0 +1,19 @@
+export const standard = defineScenario({
+  post: {
+    one: {
+      data: {
+        title: 'String',
+        body: 'String',
+        updatedAt: '2022-07-08T20:59:32Z',
+      },
+    },
+
+    two: {
+      data: {
+        title: 'String',
+        body: 'String',
+        updatedAt: '2022-07-08T20:59:32Z',
+      },
+    },
+  },
+})
